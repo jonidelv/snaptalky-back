4cab6a2d83f45b2fb30ff7238d63a6610926e889
Jonatan del Valle <delvallejonatan@gmail.com>
2024-10-29 12:19:07 -0300

    fix url db connection

--- database/database.go
+++ database/database.go
@@ -15,16 +15,47 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// Load environment variables from .env file
-	dbHost := os.Getenv("DB_HOST")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	dbPort := os.Getenv("DB_PORT")
+	// Check if DB_URL is set
+	dbURL := os.Getenv("DB_URL")
+	var dsn string
 
-	// Create the DSN (Data Source Name)
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		dbHost, dbUser, dbPassword, dbName, dbPort)
+	if dbURL != "" {
+		dsn = dbURL
+	} else {
+		// Load individual environment variables
+		dbHost := os.Getenv("DB_HOST")
+		dbUser := os.Getenv("DB_USER")
+		dbPassword := os.Getenv("DB_PASSWORD")
+		dbName := os.Getenv("DB_NAME")
+		dbPort := os.Getenv("DB_PORT")
+
+		// Validate required environment variables
+		missingVars := []string{}
+		if dbHost == "" {
+			missingVars = append(missingVars, "DB_HOST")
+		}
+		if dbUser == "" {
+			missingVars = append(missingVars, "DB_USER")
+		}
+		if dbPassword == "" {
+			missingVars = append(missingVars, "DB_PASSWORD")
+		}
+		if dbName == "" {
+			missingVars = append(missingVars, "DB_NAME")
+		}
+		if dbPort == "" {
+			missingVars = append(missingVars, "DB_PORT")
+		}
+
+		if len(missingVars) > 0 {
+			utils.LogError(fmt.Errorf("missing environment variables: %v", missingVars), "Failed to construct DSN")
+			return
+		}
+
+		// Create the DSN (Data Source Name)
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			dbHost, dbUser, dbPassword, dbName, dbPort)
+	}
 
 	// Determine the logger configuration based on the environment
 	var config *gorm.Config
